9fd9e5c3c0c32f7ffbf68794a89c54b27e6a193a
boypro0409 <boypro0409@gmail.com>
2022-05-11 01:57:36 +0700

    Bài 3: Require statement, custom modifier

--- contracts/MyPet.sol
+++ contracts/MyPet.sol
@@ -3,12 +3,22 @@ pragma solidity ^0.8.0;
 
 contract MyPets {
   string public MyDog;
+  address public Owner;
 
+  modifier onlyOwner () {
+    require(msg.sender == Owner, "Authorized user only");
+    _;
+  }
   constructor(string memory _myDog) {
     MyDog = _myDog;
+    Owner = msg.sender;
   }
 
-  function updateDog(string memory _myDog) external {
+  function changeOwner (address _newOwner) external onlyOwner {
+    Owner = _newOwner;
+  }
+  function updateDog(string memory _myDog) external onlyOwner {
+    require(msg.sender == Owner, "Authorized user only");
     MyDog = _myDog;
   }
 }
--- test/test_MyPet.js
+++ test/test_MyPet.js
@@ -3,20 +3,42 @@ const { ethers } = require('hardhat');
 
 describe('Access Control', () => {
     let deployer, attacker, user;
-
     beforeEach(async function () {
         [deployer, attacker, user] = await ethers.getSigners();
         const MyPets = await ethers.getContractFactory('MyPets', deployer);
         this.myPets = await MyPets.deploy('Lu');
+        // console.log([deployer.address]);
+        // console.log([user.address]);
+        // console.log([attacker.address]);
     });
     describe('My Pets', () => {
         it('Should set dog name at deployment', async function () {
             expect(await this.myPets.MyDog()).to.eq('Lu');
         });
 
-        it('Anyone can change my dog name', async function () {
-            await this.myPets.connect(attacker).updateDog('kiki');
-            expect(await this.myPets.MyDog()).to.eq('kiki');
+        it('Should set the deployer account as owner', async function () {
+            expect(await this.myPets.Owner()).to.eq(deployer.address);
+        });
+
+        it('Should NOT be able to change the dog name if not the owner', async function () {
+            await expect(this.myPets.connect(attacker).updateDog('kiki')).to.be.revertedWith('Authorized user only');
+        });
+
+        it('Should be possible for owner transfer ownership', async function () {
+            await this.myPets.changeOwner(user.address);
+            expect(await this.myPets.Owner()).to.eq(user.address);
+        });
+
+        it('Should be possible for the new owner update the dog name', async function () {
+            await this.myPets.changeOwner(user.address);
+            await this.myPets.connect(user).updateDog('Kaka');
+            expect(await this.myPets.MyDog()).to.eq('Kaka');
+        });
+
+        it('Should NOT be possible for the others to transfer ownership', async function () {
+            await expect(this.myPets.connect(attacker).changeOwner(attacker.address)).to.be.revertedWith(
+                'Authorized user only',
+            );
         });
     });
 });
