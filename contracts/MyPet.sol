180e2bb22c2560f2279f51513999201597cc3b11
boypro0409 <boypro0409@gmail.com>
2022-05-11 02:07:10 +0700

    Bài 4: Solving access control by community vetted code

--- contracts/MyPet.sol
+++ contracts/MyPet.sol
@@ -1,24 +1,15 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyPets {
+contract MyPets is Ownable{
   string public MyDog;
-  address public Owner;
 
-  modifier onlyOwner () {
-    require(msg.sender == Owner, "Authorized user only");
-    _;
-  }
   constructor(string memory _myDog) {
     MyDog = _myDog;
-    Owner = msg.sender;
   }
 
-  function changeOwner (address _newOwner) external onlyOwner {
-    Owner = _newOwner;
-  }
   function updateDog(string memory _myDog) external onlyOwner {
-    require(msg.sender == Owner, "Authorized user only");
     MyDog = _myDog;
   }
 }
--- test/test_MyPet.js
+++ test/test_MyPet.js
@@ -17,27 +17,29 @@ describe('Access Control', () => {
         });
 
         it('Should set the deployer account as owner', async function () {
-            expect(await this.myPets.Owner()).to.eq(deployer.address);
+            expect(await this.myPets.owner()).to.eq(deployer.address);
         });
 
         it('Should NOT be able to change the dog name if not the owner', async function () {
-            await expect(this.myPets.connect(attacker).updateDog('kiki')).to.be.revertedWith('Authorized user only');
+            await expect(this.myPets.connect(attacker).updateDog('kiki')).to.be.revertedWith(
+                'Ownable: caller is not the owner',
+            );
         });
 
         it('Should be possible for owner transfer ownership', async function () {
-            await this.myPets.changeOwner(user.address);
-            expect(await this.myPets.Owner()).to.eq(user.address);
+            await this.myPets.transferOwnership(user.address);
+            expect(await this.myPets.owner()).to.eq(user.address);
         });
 
         it('Should be possible for the new owner update the dog name', async function () {
-            await this.myPets.changeOwner(user.address);
+            await this.myPets.transferOwnership(user.address);
             await this.myPets.connect(user).updateDog('Kaka');
             expect(await this.myPets.MyDog()).to.eq('Kaka');
         });
 
         it('Should NOT be possible for the others to transfer ownership', async function () {
-            await expect(this.myPets.connect(attacker).changeOwner(attacker.address)).to.be.revertedWith(
-                'Authorized user only',
+            await expect(this.myPets.connect(attacker).transferOwnership(attacker.address)).to.be.revertedWith(
+                'Ownable: caller is not the owner',
             );
         });
     });
